e32379e74397376da3c26e3c71a44f1a39368fb8
atuhin <atuhinbd@gmail.com>
2023-01-23 23:56:56 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import { getAuth, GoogleAuthProvider, signInWithPopup  } from 'firebase/auth';
+import app from './Firebase/Firbase.init';
 
-function App() {
+const auth = getAuth();
+
+function App() { 
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSingIn = () => {
+    signInWithPopup(auth, provider)
+     signInWithPopup(auth, )
+    .then((result) => {
+        console.log(result);
+      })
+    .catch((error) => {
+        console.log("error: ", error);
+  })
+}
   return (
     <div className="App">
+
+      <button onClick={handleGoogleSingIn}>Google</button>
      
     </div>
   );
--- src/Firebase/Firbase.init.js
+++ src/Firebase/Firbase.init.js
@@ -1,11 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDHGhGeADc19E91mu5gI6gw3-EBYVUO2JY",
   authDomain: "fir-fbd57.firebaseapp.com",
@@ -16,8 +10,6 @@ const firebaseConfig = {
   measurementId: "G-SHKVHDH6D4"
 };
 
-
-<h1>Thi is firebase </h1>
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export default app;
